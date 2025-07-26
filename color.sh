0f94f604ce6b2193755120b6084a46d7f10515b7
Andrey <firesist@yandex.ru>
2025-07-26 13:05:57 +0300

    Create color.sh

---
+++ color.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+CYAN="\e[36m"
+BOLDGREEN="\e[1;${GREEN}m"
+ITALICRED="\e[3;${RED}m"
+ENDCOLOR="\e[0m"
+
+echo ''
+echo -e "${CYAN}############################################${ENDCOLOR}"
+echo -e "${CYAN}############################################${ENDCOLOR}"
+echo -e "${CYAN}############################################${ENDCOLOR}"
+echo -e "${CYAN}############################################${ENDCOLOR}"
+echo ''
